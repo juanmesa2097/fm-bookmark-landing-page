955886265e91c9d649e9970473b8c445dab1095c
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-28 18:15:55 -0500

    Add reusable button's classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Rubik", "sans-serif"],
+      body: ["Rubik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "soft-blue": "#5368df",
+        "soft-red": "#fa5757",
+        "grayish-blue": "#9194a1",
+        "dark-blue": "#252b46",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
