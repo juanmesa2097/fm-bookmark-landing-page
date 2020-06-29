8a3a44ade72a829ebeccb5f23b144f56932e3b96
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-29 01:13:40 -0500

    Create tabs section

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,23 @@
+// navbar
+
 const nav = document.querySelector("#nav");
 
 function onClickOpenMenu() {
-  nav.classList.add("is-open");
+  nav.classList.add("is-navbar-open");
 }
 
 function onClickCloseMenu() {
-  nav.classList.remove("is-open");
+  nav.classList.remove("is-navbar-open");
+}
+
+// tabs
+const tabs = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".tab-content");
+const activeClass = "border-soft-red";
+
+function onClickTab(e, target) {
+  tabs.forEach((tab) => (tab.className = tab.className.replace(activeClass)));
+  contents.forEach((tab) => tab.classList.remove("is-tab-visible"));
+  e.target.classList.add(activeClass);
+  document.querySelector(target).classList.add("is-tab-visible");
 }
