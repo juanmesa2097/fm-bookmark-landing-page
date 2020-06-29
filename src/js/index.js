1caf24ac3439954adaa9421f676d5ea37d6c148c
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-29 15:48:02 -0500

    Create email subscription section

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// navbar
+// Tavbar
 
 const nav = document.querySelector("#nav");
 
@@ -10,7 +10,7 @@ function onClickCloseMenu() {
   nav.classList.remove("is-navbar-open");
 }
 
-// tabs
+// Tabs
 const tabs = document.querySelectorAll(".tab-button");
 const contents = document.querySelectorAll(".tab-content");
 const activeClass = "border-soft-red";
@@ -24,6 +24,45 @@ function onClickTab(e, target) {
   );
   contents.forEach((tab) => tab.classList.remove("is-tab-visible"));
   e.target.className += " border-soft-red font-medium";
-  console.log(e.target);
   document.querySelector(target).classList.add("is-tab-visible");
 }
+
+// Subscription
+const emailWrapper = document.querySelector("#emailWrapper");
+const inputField = emailWrapper.querySelector("input");
+const message = emailWrapper.querySelector("p");
+
+function validateEmail(email) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+function showFieldError() {
+  inputField.classList.add("border-soft-red");
+  message.classList.remove("hidden");
+}
+
+function removeFieldError() {
+  inputField.classList.remove("border-soft-red");
+  message.classList.add("hidden");
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const serialized = {};
+  const data = new FormData(form);
+
+  for (const [name, value] of data) {
+    serialized[name] = value;
+  }
+
+  const { email } = serialized;
+
+  if (validateEmail(email)) {
+    removeFieldError();
+    alert("You'll recive the latest updates of BOOKMARK");
+  } else {
+    showFieldError();
+  }
+});
